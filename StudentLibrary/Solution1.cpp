da0ce68e32cea093b686b6b578efbc0dcdcec7a4
Portos2004 <130547006+Portos2004@users.noreply.github.com>
2024-01-19 14:16:33 +0100

    SuisMauvaisDesole (2/2)
    Et en plus il marche pas

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,42 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+    if (_from == _to) {
+        return _value; 
+    }
+
+    switch (_from) {
+    case TemperatureUnits::CELSIUS:
+        if (_to == TemperatureUnits::FAHRENHEIT) {
+            return (_value * 9 / 5) + 32;
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            return _value + 273.15f;
+        }
+        break;
+
+    case TemperatureUnits::FAHRENHEIT:
+        if (_to == TemperatureUnits::CELSIUS) {
+            return (_value - 32) * 5 / 9;
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            return (_value + 459.67f) * 5 / 9;
+        }
+        break;
+
+    case TemperatureUnits::KELVIN:
+        if (_to == TemperatureUnits::CELSIUS) {
+            return _value - 273.15f;
+        }
+        else if (_to == TemperatureUnits::FAHRENHEIT) {
+            return (_value * 9 / 5) - 459.67f;
+        }
+        break;
+    }
+
+    return -1.0f; 
 }
+
+
 
 #endif
